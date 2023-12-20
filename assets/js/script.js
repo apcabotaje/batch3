dd2c52386b1a8cf1813549f45d7c1c7b23461ec8
apcabotaje <aymancabotaje.dev@gmail.com>
2023-12-20 20:30:08 +0800

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const body = document.body
+
+
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+spanHi.style.color = "red"
+spanBye.style.backgroundColor = "blue"
